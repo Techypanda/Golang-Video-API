bd0dc9bdfa9f8b1186c0e319398778462701a4b1
Techypanda <Jonathan_wright@hotmail.com>
2021-10-23 21:05:16 +0800

    move back to 1323

--- src/go.mod
+++ src/go.mod
@@ -5,6 +5,5 @@ go 1.16
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/labstack/echo/v4 v4.6.1
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- src/server.go
+++ src/server.go
@@ -1,44 +1,13 @@
 package main
 
 import (
-	"crypto/tls"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"golang.org/x/crypto/acme"
-	"golang.org/x/crypto/acme/autocert"
 )
-
-func useHttps(e *echo.Echo) {
-	autoTLSManager := autocert.Manager{
-		Prompt: autocert.AcceptTOS,
-		// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
-		Cache: autocert.DirCache("/var/www/.cache"),
-		//HostPolicy: autocert.HostWhitelist("<DOMAIN>"),
-	}
-	s := http.Server{
-		Addr:    ":443",
-		Handler: e, // set Echo as handler
-		TLSConfig: &tls.Config{
-			//Certificates: nil, // <-- s.ListenAndServeTLS will populate this field
-			GetCertificate: autoTLSManager.GetCertificate,
-			NextProtos:     []string{acme.ALPNProto},
-		},
-		//ReadTimeout: 30 * time.Second, // use custom timeouts
-	}
-	if err := s.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
-		e.Logger.Fatal(err)
-	} else {
-		e.Logger.Fatal("Failed To Start HTTPS")
-		e.Logger.Fatal(err)
-	}
-}
-
-func useHttp(e *echo.Echo) {
-	e.Logger.Fatal(e.Start(":80"))
-}
 
 func main() {
 	e := echo.New()
@@ -50,10 +19,5 @@ func main() {
 			<h3>TLS certificates automatically installed from Let's Encrypt :)</h3>
 		`)
 	})
-
-	if os.Getenv("HTTPS") == "TRUE" {
-		useHttps(e)
-	} else {
-		useHttp(e)
-	}
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
 }
