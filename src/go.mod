a18920e6a0aac95c5f10b1cf76ce22ad84e7cff8
Techypanda <Jonathan_wright@hotmail.com>
2021-10-23 20:34:49 +0800

    add certbot

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,8 @@ module tiktok.techytechster.com
 go 1.16
 
 require (
-	github.com/gin-gonic/autotls v0.0.3
-	github.com/gin-gonic/gin v1.7.4
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/labstack/echo/v4 v4.6.1
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- src/server.go
+++ src/server.go
@@ -1,19 +1,59 @@
 package main
 
 import (
-	"log"
+	"crypto/tls"
 	"net/http"
+	"os"
 
-	"github.com/gin-gonic/autotls"
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/crypto/acme"
+	"golang.org/x/crypto/acme/autocert"
 )
 
+func useHttps(e *echo.Echo) {
+	autoTLSManager := autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
+		Cache: autocert.DirCache("/var/www/.cache"),
+		//HostPolicy: autocert.HostWhitelist("<DOMAIN>"),
+	}
+	s := http.Server{
+		Addr:    ":443",
+		Handler: e, // set Echo as handler
+		TLSConfig: &tls.Config{
+			//Certificates: nil, // <-- s.ListenAndServeTLS will populate this field
+			GetCertificate: autoTLSManager.GetCertificate,
+			NextProtos:     []string{acme.ALPNProto},
+		},
+		//ReadTimeout: 30 * time.Second, // use custom timeouts
+	}
+	if err := s.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
+		e.Logger.Fatal(err)
+	} else {
+		e.Logger.Fatal("Failed To Start HTTPS")
+		e.Logger.Fatal(err)
+	}
+}
+
+func useHttp(e *echo.Echo) {
+	e.Logger.Fatal(e.Start(":80"))
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, `
+			<h1>Welcome to Echo!</h1>
+			<h3>TLS certificates automatically installed from Let's Encrypt :)</h3>
+		`)
 	})
-	log.Fatal(autotls.Run(r, "tiktok.techytechster.com"))
+
+	if os.Getenv("HTTPS") == "TRUE" {
+		useHttps(e)
+	} else {
+		useHttp(e)
+	}
 }
